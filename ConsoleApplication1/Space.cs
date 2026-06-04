de7153083e9fb1fd0f4b2b7d4a3c62cf963ec92f
Userasdasd <User@DESKTOP-9ND60N2>
2026-06-04 20:12:32 +0300

    Check input of matrix params

--- ConsoleApplication1/Space.cs
+++ ConsoleApplication1/Space.cs
@@ -4,19 +4,36 @@ namespace ConsoleApplication1
 {
     internal class Space
     {
+        static Boolean CheckSize(int  size) 
+        {
+            if (size < 2 || size > 100)
+            {
+                return false;
+            } 
+            return true;
+        }
         public static void Main(string[] args)
         {
+            int[] rows = { };
+            int[] cols = { };
+            
             var rowsInput = Convert.ToInt32(Console.ReadLine());
             var colsInput = Convert.ToInt32(Console.ReadLine());
             
-            int[] rows = new int[rowsInput];
-            int[] cows = new int [colsInput];
-
-            for (int i = 0; i < cows.Length; i++)
+            if (CheckSize(rowsInput)){
+                rows = new int[rowsInput];
+            }
+            
+            if (CheckSize(colsInput)){
+                cols = new int [colsInput];
+            }
+            
+            // Matrix 2 dimension
+            for (int i = 0; i < cols.Length; i++)
             {
                 for (int j = 0; j < rows.Length; j++)
                 {
-                    Console.Write(cows[j] + " ");
+                    Console.Write(cols[j] + " ");
                 }
 
                 Console.WriteLine("");
